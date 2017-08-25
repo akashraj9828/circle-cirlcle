be29bcef46c6f63f497c642e9b1ee294c1c20898
AKASH RAJ <akashraj9828@users.noreply.github.com>
2017-08-25 23:54:25 +0530

    added cirlce trigger

--- sketch.js
+++ sketch.js
@@ -14,6 +14,7 @@ var ptx=[]
 var pty=[]
 var ang=[]
 var r=[]
+var circle=true;
 function setup() {
 createCanvas(500,500)
 background(0)
@@ -30,34 +31,39 @@ angle=[0,0.001,0.002,0.003,0.004]
 }
 
 function draw() {
-
-  // background(51)
+  if(circle)
+  background(51)
  // ang+=angSlider.value();
  // ang2+=angSliderr.value();
+//  if(!circle){
  push()
 noFill()
 rotate(ang)
 translate(width/2,height/2)
-
-// ellipse(0,0,r1*2,r1*2)
+if(circle)
+ellipse(0,0,r1*2,r1*2)
 rotate(ang1)
 translate(0,r1-r2)
-// ellipse(0,0,r2*2,r2*2)
+if(circle)
+ellipse(0,0,r2*2,r2*2)
 rotate(ang2)
 translate(0,r2-r3)
 stroke(225)
 strokeWeight(2)
 point(0,0)
-// ellipse(0,0,r3*2,r3*2)
+if(circle)
+ellipse(0,0,r3*2,r3*2)
 rotate(ang3)
 translate(0,r3-r4)
 stroke(225)
 strokeWeight(2)
 point(0,0)
-// ellipse(0,0,r4*2,r4*2)
+if(circle)
+ellipse(0,0,r4*2,r4*2)
 rotate(ang4)
 translate(0,r4-r5)
-// ellipse(0,0,r5*2,r5*2)
+if(circle)
+ellipse(0,0,r5*2,r5*2)
 // for(i=0;i<5;i++)
 // cir(i+1)
 push()
@@ -65,6 +71,9 @@ stroke(225)
 strokeWeight(2)
 point(0,0)
 pop()
+//  }
+
+
 ang=ang+0.001
 ang1+=0.1
 ang2+=0.01
