b70947fa88f88bb362337f88f55a5855a2e67656
AKASH RAJ <akashraj9828@users.noreply.github.com>
2017-08-26 10:01:29 +0530

    cirlce added in loop| radius genralized | rotation genralized | cirlce inside or outside option added

--- sketch.js
+++ sketch.js
@@ -1,31 +1,39 @@
-var r1=200
-var r2=50
-var r3=100
-var r4=50
-var r5=40
-var ang=0.001
-var ang1=0
-var ang2=0
-var ang3=0
-var ang4=0
+var r1=30
+
+var ang=10
+
 var angSlider
 var angSliderr
 var ptx=[]
 var pty=[]
-var ang=[]
+var angle=[]
 var r=[]
 var circle_visible=true;
 var circle_checkbox
 var speed
 var pointweight=4
+var n=10;
+var rslider
+var out=false
+
+
 function setup() {
-createCanvas(500,500)
-background(0)
+createCanvas(windowWidth,windowHeight)
+background(51)
 angSlider=createSlider(0,1,0.2,0.01)
 angSliderr=createSlider(0,1,0.2,0.01)
+rslider=createSlider(10,500,200,1)
 speed=createSlider(5,60,35,2)
 circle_checkbox=createCheckbox("toggle circle")
 circle_checkbox.changed(bg)
+for(i=0;i<n;i++){
+  
+  angle[i]=ang
+  ang=ang/random(10,20)
+
+  var circle_visible=true
+}
+
 // circle_checkbox.positon(700,700)
 
 // circle_checkbox = createCheckbox(" fill");
@@ -37,24 +45,31 @@ circle_checkbox.changed(bg)
 angleMode(RADIANS)
 // frameRate(1)
 
-r=[200,10,50,25,12]
-angle=[0,0.001,0.002,0.003,0.004]
+// r=[200,10,50,25,12]
+// angle=[0,0.001,0.002,0.003,0.004]
 
 }
 
 function bg(){
   background(51)
 }
-
+function setradius(){
+  for(i=0;i<n;i++){
+    r[i]=r1
+    r1=r1/2
+  }
+}
 function draw() {
+  r1=rslider.value()
+  setradius()
 
   frameRate(speed.value())
 
   if(circle_checkbox.checked())
-    circle_visible=true
+    circle_visible=false
 
   if(!circle_checkbox.checked())
-    circle_visible=false
+    circle_visible=true
   // if(circle_checkbox.changed())
     // background(51)
   if(circle_visible)
@@ -68,45 +83,61 @@ stroke(255)
 strokeWeight(pointweight)
 rotate(ang)
 translate(width/2,height/2)
-if(circle_visible)
-ellipse(0,0,r1*2,r1*2)
-rotate(ang1)
-translate(0,r1-r2)
-if(circle_visible)
-ellipse(0,0,r2*2,r2*2)
-rotate(ang2)
-translate(0,r2-r3)
-stroke(225)
-strokeWeight(pointweight)
-point(0,0)
-if(circle_visible)
-ellipse(0,0,r3*2,r3*2)
-rotate(ang3)
-translate(0,r3-r4)
-stroke(225)
-strokeWeight(pointweight)
-point(0,0)
-if(circle_visible)
-ellipse(0,0,r4*2,r4*2)
-rotate(ang4)
-translate(0,r4-r5)
-if(circle_visible)
-ellipse(0,0,r5*2,r5*2)
+
+
+
+for (i=0;i<n;i++){
+  if(circle_visible)
+    ellipse(0,0,r[i]*2,r[i]*2)
+    rotate(angle[i])
+    angle[i]+=(i+1)/100
+    
+    if(out)
+    translate(0,r[i]*1.5)
+    if(!out)
+      translate(0,r[i]*0.5)
+    
+}
+ellipse(0,0,5,5)
+// if(circle_visible)
+// ellipse(0,0,r1*2,r1*2)
+// rotate(ang1)
+// translate(0,r1-r2)
+// if(circle_visible)
+// ellipse(0,0,r2*2,r2*2)
+// rotate(ang2)
+// translate(0,r2-r3)
+// stroke(225)
+// strokeWeight(pointweight)
+// point(0,0)
+// if(circle_visible)
+// ellipse(0,0,r3*2,r3*2)
+// rotate(ang3)
+// translate(0,r3-r4)
+// stroke(225)
+// strokeWeight(pointweight)
+// point(0,0)
+// if(circle_visible)
+// ellipse(0,0,r4*2,r4*2)
+// rotate(ang4)
+// translate(0,r4-r5)
+// if(circle_visible)
+// ellipse(0,0,r5*2,r5*2)
 // for(i=0;i<5;i++)
 // cir(i+1)
-push()
-stroke(225)
-strokeWeight(pointweight)
-point(0,0)
-pop()
+// push()
+// stroke(225)
+// strokeWeight(pointweight)
+// point(0,0)
+// pop()
 //  }
 
 
-ang=ang+0.001
-ang1+=0.1
-ang2+=0.01
-ang3+=0.13
-ang4+=0.04
+// ang=ang+0.001
+// ang1+=0.1
+// ang2+=0.01
+// ang3+=0.13
+// ang4+=0.04
 // clear()
 pop()
 
