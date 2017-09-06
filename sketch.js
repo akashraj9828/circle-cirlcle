48d73e9cde738e0ba884f29d41d11f50c8105346
AKASH RAJ <akashraj9828@gmail.com>
2017-09-06 17:38:18 +0530

    customization,UI improvement,algo cleared

--- sketch.js
+++ sketch.js
@@ -10,7 +10,7 @@ var angle=[]
 var r=[]
 var circle_visible=true;
 var circle_checkbox
-var speed
+var fps
 var pointweight=4
 var n=4;
 var rslider
@@ -21,36 +21,46 @@ var circle_visible=true
 function setup() {
 createCanvas(windowWidth,windowHeight)
 background(51)
-angSlider=createSlider(0,1,0.2,0.01)
-angSliderr=createSlider(0,1,0.2,0.01)
+
+
+fps=createSlider(5,60,35,2)
+fps.position(60,50)
+text("Fps:",15,67)
+
 rslider=createSlider(10,500,200,1)
-speed=createSlider(5,60,35,2)
+rslider.position(60,80)
+text("Radius:",15,95)
+
+scl=createSlider(0,10,1,0.01)
+scl.position(60,115)
+text("Scale:",15,130)
+
+angoffset=createSlider(0,10,1,0.01)
+angoffset.position(61,145)
+text("AngleBoost",1,160)
+
+alpha=createSlider(0,255,1,0.1)
+alpha.position(60,175)
+text("Alpha:",15,190)
+
+
+
 circle_checkbox=createCheckbox("toggle circle")
-circle_checkbox.changed(bg)
+circle_checkbox.changed(clear_bg);
+trace_center=createCheckbox("trace center")
 for(i=0;i<n;i++){
   
   angle[i]=ang*3
   ang=ang/10
 
 }
-
-// circle_checkbox.positon(700,700)
-
-// circle_checkbox = createCheckbox(" fill");
-//  circle_checkbox.changed(changeFill);
-// *   cnv = createCanvas(100, 100);
-
-// ang+=angSlider.value()
-// ang2+=ang2Slider.value()
 angleMode(RADIANS) //set angle mode to radians default is degree
-// frameRate(1)
 
-// r=[200,10,50,25,12]
-// angle=[0,0.001,0.002,0.003,0.004]
+
 
 }
 
-function bg(){
+function clear_bg(){
   background(51)
 }
 function setradius(){  // function that set radius for all the circles excluding the main bigger circle
@@ -62,35 +72,47 @@ function setradius(){  // function that set radius for all the circles excluding
 function draw() {
   r1=rslider.value()
   setradius()
+  s=scl.value()
+trace=false
+  angB=angoffset.value()/70;
+ 
 
-  frameRate(speed.value())    // changes framerate live using slider input
+  frameRate(fps.value())    // changes framerate live using slider input
 
   if(circle_checkbox.checked())
     circle_visible=true
 
   if(!circle_checkbox.checked())
     circle_visible=false
-  // if(circle_checkbox.changed())
-    // background(51)
+
+  if(trace_center.checked())
+    trace=true
+  if(!trace_center.checked())
+    trace=false
   if(circle_visible)
   background(51)
- // ang+=angSlider.value();
- // ang2+=angSliderr.value();
-//  if(!circle_visible){
  push()
 noFill()
 stroke(255)
 strokeWeight(pointweight)
 rotate(ang)
+
 translate(width/2,height/2)
 
+scale(s)
 
 
 for (i=0;i<n;i++){
-  if(circle_visible)
-    ellipse(0,0,r[i]*2,r[i]*2)
+  if(circle_visible){
+    ellipse(0,0,r[i]*2,r[i]*2)}
+    else{
+      push()
+      stroke(255,255,255,alpha.value())
+      ellipse(0,0,r[i]*2,r[i]*2)
+      pop()
+    }
     rotate(angle[i])
-    angle[i]+=(i+1)/100
+    angle[i]+=angB/(i+1)
     
     if(out)   //if circles rotates outside the main circle
     translate(0,r[i]*1.5)
@@ -98,47 +120,8 @@ for (i=0;i<n;i++){
       translate(0,r[i]*0.5)
     
 }
-ellipse(0,0,5,5)
-// if(circle_visible)
-// ellipse(0,0,r1*2,r1*2)
-// rotate(ang1)
-// translate(0,r1-r2)
-// if(circle_visible)
-// ellipse(0,0,r2*2,r2*2)
-// rotate(ang2)
-// translate(0,r2-r3)
-// stroke(225)
-// strokeWeight(pointweight)
-// point(0,0)
-// if(circle_visible)
-// ellipse(0,0,r3*2,r3*2)
-// rotate(ang3)
-// translate(0,r3-r4)
-// stroke(225)
-// strokeWeight(pointweight)
-// point(0,0)
-// if(circle_visible)
-// ellipse(0,0,r4*2,r4*2)
-// rotate(ang4)
-// translate(0,r4-r5)
-// if(circle_visible)
-// ellipse(0,0,r5*2,r5*2)
-// for(i=0;i<5;i++)
-// cir(i+1)
-// push()
-// stroke(225)
-// strokeWeight(pointweight)
-// point(0,0)
-// pop()
-//  }
-
-
-// ang=ang+0.001
-// ang1+=0.1
-// ang2+=0.01
-// ang3+=0.13
-// ang4+=0.04
-// clear()
+if(trace)
+ellipse(0,0,3,3)
 pop()
 
 
